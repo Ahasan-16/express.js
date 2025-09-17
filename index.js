1980b0c56db8c43a6f1e00f123cecfb3c5b71550
Ahasan-16 <mdahasan1916@gmail.com>
2025-09-18 01:39:05 +0600

    response related

--- index.js
+++ index.js
@@ -1,25 +1,82 @@
 var express = require('express');
-var server=express();
+var app= express();
 
-
-server.get('/',function(req,res){
-    res.send("this is home page");
+//simple string response get otoba post er sahajje ,res parameter diye send er maddome response dea jai
+app.get('/one',function(req,res){
+    res.send('this is simple response');//ei ta res er body
 });
 
-server.post('/about',function(req,res){
-    res.send("this is about page");
+//response status code manipulation
+app.get('/two',function(req,res){
+    res.status(406).end();
 });
 
-server.put('/contact',function(req,res){
-    res.end("this is contact page");
+//kibhabe json response niye asbo
+app.get('/three',function(req,res){
+
+    let myJSONArry=[
+        {
+            name:"Ahasan",
+            city:"chattogram",
+            age:"25"
+        },
+        {
+            name:"B",
+            city:"dhaka",
+            age:"26"
+        },
+        {
+            name:"c",
+            city:"Rajshai",
+            age:"27"
+        }
+    ]
+    res.json(myJSONArry);//ei khane json arry pass kora lage, arrry take onk golo json object takte pare
 });
 
-server.delete('/terms',function(req,res){
-    res.end("this is terms page");
+//kibhabe download response create korbo
+
+app.get('/four',function(req,res){
+    res.download("./download/OIP.webp");
 });
 
+// kibhabe response redirect korbo
+
+app.get('/five',function(req,res){
+   //one path e redire korbe
+    res.redirect("http://localhost:8080/one");
+});
+
+//response header e kibhabe data rakbo
+
+app.get('/six',function(req,res){
+    //ei khane key value er majkane comma takbe dui tai double quote er moddeh
+    res.append("name","Mohammad Ahasan");
+    res.append("city","Chattogram");
+
+    //ei dea laage
+    res.status(200).end();
+})
+
+// response e kibhabe cookie set korbo
+app.get('/seven',function(req,res){
+    // res.cookie("name","Mohammad Ahasan");
+    res.cookie("city","Chattogram");
+    res.cookie("age","25");
+
+    res.status(200).end("cookies set successfully.");
+})
+
+// kibhabe cookie teke data delete korbo
+app.get('/eight',function(req,res){
+    res.clearCookie("city");
+    res.clearCookie("age");
+
+    res.status(200).end("cookies delete successfully.");
+
+})
 
 
-server.listen(4000,function(){
-    console.log("Server started on port 4000");
+app.listen(8080,function(){
+    console.log("Server started on port 8080");
 })
