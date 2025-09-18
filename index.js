0b593060dad37e1820ccf368c9262d17960cb067
Ahasan-16 <mdahasan1916@gmail.com>
2025-09-18 20:41:38 +0600

    file upload using post request

--- index.js
+++ index.js
@@ -1,49 +1,27 @@
-var express=require('express');
-var app=express();
-
-//simple post request
-app.post('/one',function(req,res){
-    res.end("this is simple post request");
+var express = require('express');
+var app = express();
+var multer = require("multer");
+var storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null,"./uploads");//uploads directory ta toiri kore nite hobe root directory te
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.originalname);
+    }
 });
-//kibhabe query maddome url teke data nibo
-app.post('/two',function(req,res){
-    let username=req.query.userName;
-    let password=req.query.passWord;
-    res.send(username+" "+password);
-});
-//header er data kibhabe access korbo,je gola postman e set kora take
-app.post('/three',function(req,res){
-    let username=req.header("userName");
-    let password=req.header("passWord");
-    res.send(username+" "+password);
-});
+ var upload=multer({storage:storage}).single("myfile");//myfile name ta postman er key same hobe
 
-//postman er body te json data diye oi data kibhabe niye asbo
-//npm install body-parser
-var bodyparser=require('body-parser');
-app.use(bodyparser.json());
-app.post('/four',function(req,res){
-    let jsonData=req.body;
-    //specific ekta o nite pari
-    // let name=jsonData["name"];
-    let jsonString=JSON.stringify(jsonData);
-    res.send(jsonString);
-});
+ app.post("/six",function(req,res){
+     upload(req, res,function(error){
+         if(error)
+         {
+             res.send("file upload error");
+         }else{
+             res.send("File upload success");
+         }
 
-
-//postman er body te multipart form-data niye asbo
-//npm install --save multer
-var multer=require('multer');
-var multer=multer();
-app.use(multer.array());
-app.use(express.static('public'));
-app.post('/five',function(req,res){
-     let jsonData=req.body;
-     res.send(JSON.stringify(jsonData));
-});
-
-
-
-app.listen(8080,function(){
-    console.log("Server started on port 8080");
-})
+     });
+ });
+ app.listen(8080,function(){
+     console.log("Server started on port 8080");
+ })
