7cc99ea8a952914eba7d33b6f499cb139197a7fa
Ahasan-16 <mdahasan1916@gmail.com>
2025-09-19 00:53:01 +0600

    application level middleware

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-var express = require('express');
-var app = express();
-var multer = require("multer");
-var storage = multer.diskStorage({
-    destination: function (req, file, callback) {
-        callback(null,"./uploads");//uploads directory ta toiri kore nite hobe root directory te
-    },
-    filename: function (req, file, callback) {
-        callback(null, file.originalname);
-    }
+var express=require('express');
+var app=express();
+
+
+app.use(function(req,res,next){
+    console.log("i am middle ware");
+    next();
+})
+
+
+
+app.get('/',function(req,res){
+    res.send("this is home page");
 });
- var upload=multer({storage:storage}).single("myfile");//myfile name ta postman er key same hobe
 
- app.post("/six",function(req,res){
-     upload(req, res,function(error){
-         if(error)
-         {
-             res.send("file upload error");
-         }else{
-             res.send("File upload success");
-         }
+app.get('/login',function(req,res){
+    res.send("this is login page");
+});
 
-     });
- });
- app.listen(8080,function(){
-     console.log("Server started on port 8080");
- })
+app.get('/logout',function(req,res){
+    res.send("this is logout page");
+})
+
+
+
+
+app.listen(8080,function(){
+    console.log("Server started on port 8080");
+});
